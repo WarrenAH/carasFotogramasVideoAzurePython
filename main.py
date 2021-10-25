ea0bf48af884a04e58f259c0db3472bc0e30a932
WarrenAH <92707982+WarrenAH@users.noreply.github.com>
2021-10-25 00:51:24 -0600

    estadistica

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 import requests
 import time
 import cognitive_face as CF
+import matplotlib
+matplotlib.use('Agg')
+import numpy as np
+import matplotlib.pyplot as plt
 
 from threading import Thread
 
@@ -41,7 +45,78 @@ def mostrarFotogramaCuadradoCara(imagen, analisisImagen):
     arreglarColor = cv2.cvtColor(imagenTemporal, cv2.COLOR_BGR2RGB)
     cv2.imwrite('./fotogramaCara/' + ubicacionArchivoEncontrado, arreglarColor)
 
+agregarCaras= []
+
+def crearEstadisticas():
+    global agregarCaras
+    listaDatos=[]
+    anger = 0
+    contempt = 0
+    disgust = 0
+    fear = 0
+    happiness = 0
+    neutral = 0
+    sadness = 0
+    surprise =0
+    for i in agregarCaras:
+        faceAttributes = i['faceAttributes']
+        emotions = faceAttributes['emotion']
+
+        if emotions['anger']>=0.5:
+            anger += 1
+        elif emotions['contempt'] >=0.5:
+            contempt += 1
+        elif emotions['disgust'] >=0.5:
+                disgust += 1
+        elif emotions['fear'] >=0.5:
+            fear += 1
+        elif emotions['happiness'] >=0.5:
+            happiness += 1
+        elif emotions['sadness'] >=0.5:
+            sadness += 1
+        elif emotions['surprise'] >=0.5:
+            surprise += 1
+        else:
+            neutral += 1
+
+    listaDatos.append(anger)
+    listaDatos.append(contempt)
+    listaDatos.append(disgust)
+    listaDatos.append(fear)
+    listaDatos.append(happiness)
+    listaDatos.append(neutral)
+    listaDatos.append(sadness)
+    listaDatos.append(surprise)
+    return listaDatos
+
+def func(pct, allvals):
+    absolute = int(round(pct/100.*np.sum(allvals)))
+    return "{:.1f}%\n({:d} Persona)".format(pct, absolute)
+
+def imprimirEstadisticas():
+        global agregarCaras
+        fig, ax = plt.subplots(figsize=(7, 5), subplot_kw=dict(aspect="equal"))
+
+        data = crearEstadisticas()
+        emocionesBuscadas = ["anger","contempt","disgust","fear","hapiness","neutral","sadness","surprise"]
+
+        wedges, texts, autotexts = ax.pie(data, autopct=lambda pct: func(pct, data),
+                                          textprops=dict(color="w"))
+
+        ax.legend(wedges, emocionesBuscadas,
+                  title="Emociones",
+                  loc="center left",
+                  bbox_to_anchor=(1, 0, 0.5, 1))
+
+        plt.setp(autotexts, size=7, weight="bold")
+
+        ax.set_title("Emociones detectadas en forma general")
+
+        plt.savefig('./fotogramaCara/EstadisticasEmociones.png')
+
+
 def emociones(imagen):
+  global agregarCaras
   imagenObtenida = imagen
   imagenDatos = open(imagenObtenida, "rb").read()
   encabezados = {'Ocp-Apim-Subscription-Key': llaveSuscripcionFaceApiAzure,
@@ -54,9 +129,11 @@ def emociones(imagen):
   respuesta = requests.post(
       urlSuscripcionFaceApiAzure + "detect/", headers=encabezados, params=parametros, data=imagenDatos)
   analisis = respuesta.json()
+  agregarCaras.extend(analisis)
   return analisis
 
 def emociones2(imagen):
+  global agregarCaras
   imagenObtenida = imagen
   imagenDatos = open(imagenObtenida, "rb").read()
   encabezados = {'Ocp-Apim-Subscription-Key': llaveSuscripcionFaceApiAzure2,
@@ -69,6 +146,7 @@ def emociones2(imagen):
   respuesta = requests.post(
       urlSuscripcionFaceApiAzure2 + "detect/", headers=encabezados, params=parametros, data=imagenDatos)
   analisis = respuesta.json()
+  agregarCaras.extend(analisis)
   return analisis
 
 
@@ -306,6 +384,7 @@ class revisarFotogramasCaras(Thread):
             tiempoFinalTiempoMultiproceso = time.time() - contarTiempoMultiproceso
             convertirAMinutosMultiproceso = tiempoFinalTiempoMultiproceso // 60
             convertirASegundosMultiproceso = tiempoFinalTiempoMultiproceso % 60
+            imprimirEstadisticas()
             print("")
             print("Duracion total del programa en segundos con multiproceso:", round(tiempoFinalTiempoMultiproceso, 1))
             print("Su equivalente a minutos con segundos (min : seg):", int(convertirAMinutosMultiproceso), ":",
@@ -325,6 +404,7 @@ def desicion():
         tiempoFinalTiempoSecuencial = time.time() - contarTiempoSecuencial
         convertirAMinutosSecuencial=tiempoFinalTiempoSecuencial // 60
         convertirASegundosSecuencial=tiempoFinalTiempoSecuencial % 60
+        imprimirEstadisticas()
         print("")
         print("Duracion total del programa en segundos de forma secuencial:", round(tiempoFinalTiempoSecuencial, 1))
         print("Su equivalente a minutos con segundos (min : seg):", int(convertirAMinutosSecuencial),":",int(convertirASegundosSecuencial))
